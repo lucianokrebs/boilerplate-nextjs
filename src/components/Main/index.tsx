5f0dd203d819d57ec246834fb9319cc228ab144c
Luciano Krebs <lkrebs05@gmail.com>
2021-10-03 15:27:30 -0300

    feat: improve home page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,18 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>Advanced React</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="/img/logo.svg" alt="Image of the ReactJS Logo" />
+      <S.Title>Advanced React</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS and Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="An illustration of a developer in front of a computer"
+      />
+    </S.Wrapper>
   )
 }
 
